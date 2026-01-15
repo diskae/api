533773b30be5f44c14aafb064217f18dd2427e60
Diskae <sheekov7semen@gmail.com>
2026-01-16 00:52:19 +0700

    Добавлен контроллер

--- obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
+++ obj/Debug/net8.0/CollegeSchedule.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9125a493fd523069e5ec41ff6d40ade0d67975ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552df9e20950cde1c8522ca09c7df4a5c15698ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeSchedule")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
